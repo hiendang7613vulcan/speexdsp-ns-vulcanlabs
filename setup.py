7e5b6012f4c41a2f808fafb5f0074b928a0288dd
hiendang7613vulcan <hiendang@vulcanlabs.co>
2025-12-07 16:03:04 +0700

    Fix: improve setup.py to find speexdsp in manylinux, build speexdsp from source

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os
 import sys
+import subprocess
 from glob import glob
 from setuptools import setup, Extension
 from distutils.command.build import build
@@ -9,10 +11,50 @@ from distutils.command.build import build
 with open('README.md') as f:
     long_description = f.read()
 
+
+def get_pkg_config(package, option):
+    """Get pkg-config values for a package."""
+    try:
+        result = subprocess.run(
+            ['pkg-config', option, package],
+            capture_output=True, text=True, check=True
+        )
+        return result.stdout.strip().split()
+    except (subprocess.CalledProcessError, FileNotFoundError):
+        return []
+
+
+# Base configuration
 include_dirs = ['src']
+library_dirs = []
 libraries = ['speexdsp', 'stdc++']
 define_macros = []
 extra_compile_args = []
+extra_link_args = []
+
+# Try to get paths from pkg-config
+pkg_cflags = get_pkg_config('speexdsp', '--cflags')
+pkg_libs = get_pkg_config('speexdsp', '--libs')
+
+for flag in pkg_cflags:
+    if flag.startswith('-I'):
+        include_dirs.append(flag[2:])
+
+for flag in pkg_libs:
+    if flag.startswith('-L'):
+        library_dirs.append(flag[2:])
+
+# Add common paths
+common_include_paths = ['/usr/local/include', '/usr/local/include/speex', '/usr/include', '/usr/include/speex']
+common_lib_paths = ['/usr/local/lib', '/usr/local/lib64', '/usr/lib', '/usr/lib64']
+
+for path in common_include_paths:
+    if os.path.exists(path) and path not in include_dirs:
+        include_dirs.append(path)
+
+for path in common_lib_paths:
+    if os.path.exists(path) and path not in library_dirs:
+        library_dirs.append(path)
 
 sources = (
     glob('src/noise_suppression.cpp') +
@@ -41,9 +83,11 @@ setup(
             sources=sources,
             swig_opts=swig_opts,
             include_dirs=include_dirs,
+            library_dirs=library_dirs,
             libraries=libraries,
             define_macros=define_macros,
-            extra_compile_args=extra_compile_args
+            extra_compile_args=extra_compile_args,
+            extra_link_args=extra_link_args
         )
     ],
     classifiers=[
